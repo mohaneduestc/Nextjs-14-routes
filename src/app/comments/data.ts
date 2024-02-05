96e1678357b3bb77dcb2822294185e72adc1c2f9
mohaned110 <mohaneduestc@gmail.com>
2024-02-05 22:31:20 +0400

    routes

---
+++ src/app/comments/data.ts
@@ -0,0 +1,12 @@
+export const comments = [
+    {
+        id: 1,
+        text: " comment",
+      },{
+        id: 2,
+        text: "second comment",
+      },{
+        id: 3,
+        text: "third comment",
+      },
+];
